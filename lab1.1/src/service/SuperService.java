ad4fffa9f78d5057fa38df9a142323e99d847a28
shadyman <m.m.shdmn@gmail.com>
2023-04-08 12:28:26 +0600

    add method to get GCD

--- lab1.1/src/service/SuperService.java
+++ lab1.1/src/service/SuperService.java
@@ -147,4 +147,18 @@ public class SuperService {
             return getCoef(n-1, k-1) + getCoef(n-1, k);
         }
     }
+
+    /**
+        @getGcd - find gcd of 2 numbers
+        @param1 - integer number (a)
+        @param2 - integer number (b)
+        @return - gcd (integer)
+    **/
+    public static int getGcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return getGcd(b, a % b);
+        }
+    }
 }
