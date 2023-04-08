eba46e508f6583c4527300f0e5eca0b0a613db53
shadyman <m.m.shdmn@gmail.com>
2023-04-08 12:08:38 +0600

    add method to find minimum

--- lab1.1/src/service/SuperService.java
+++ lab1.1/src/service/SuperService.java
@@ -1,4 +1,22 @@
 package service;
 
+import java.util.Scanner;
+
 public class SuperService {
+    /**
+     @findMin - To find the minimum in a list
+     @param - integer number (N)
+     @return - the smallest array object(integer)
+     **/
+    public static void findMin(int N){
+        int[] array = new int[N];
+        int min = 99999999;
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < N; i++){
+            array[i] = scanner.nextInt();
+            if (min>array[i])
+                min = array[i];
+        }
+        System.out.println(min);
+    }
 }
